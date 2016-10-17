cc94a07fb6e12ae2a611ce7a29ef1d34d401893c
palaviv <palaviv@gmail.com>
2016-10-18 00:15:28 +0300

    Add setup.py file

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+
+setup(
+    name="caster",
+    version="0.1",
+    py_modules=['caster'],
+    author="Aviv Palivoda",
+    author_email="palaviv@gmail.com",
+    description="Caster is a command line tool for casting media to chromecast",
+    license="MIT",
+    url="https://github.com/palaviv/caster",
+    entry_points={
+        'console_scripts': ['caster=caster:main'],
+    },
+    install_requires=[
+        'six',
+        'pychromecast'
+    ],
+)
